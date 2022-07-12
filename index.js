d4c907d11607d0e8275c319425ee2a0888ef6360
harli-lacej <harlilacej16@gmail.com>
2022-07-12 18:36:09 +0200

    enter trigger to create a div for each 'todo'

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let text_field = document.getElementById("fname");
 text_field.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    let newdivelem = document.createElement('div');
-    newdivelem.classList.add('todo-div-todo');
-    newdivelem.textContent = 'Replacement text';
-    
-    
+  if(event.key === "Enter") {
+    //event.preventDefault();
+    console.log("Enter pressed");
+    let block_to_insert = document.createElement('div');
+    let text_to_insert = document.getElementById('fname').value;
+    block_to_insert.innerHTML = ''+text_field.value;
+    block_to_insert.classList.add('todo-div-todo');
+ 
+    let where_to_insert = document.getElementById('outer-div');
+    where_to_insert.appendChild( block_to_insert );
   }
 });
