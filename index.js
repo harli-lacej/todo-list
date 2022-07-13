0c78bbbae6e2580bbf656e0bc65ff3a671917e78
harli-lacej <harlilacej16@gmail.com>
2022-07-13 11:43:28 +0200

    delete icon on mouse over

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ text_field.addEventListener("keypress", function(event) {
     //event.preventDefault();
     console.log("Enter pressed");
     let block_to_insert = document.createElement('div');
+    block_to_insert.id = "div-created";
     let text_to_insert = document.getElementById('fname').value;
     let text=block_to_insert.innerHTML = ''+text_field.value;
     block_to_insert.classList.add('todo-div-todo');
@@ -15,7 +16,14 @@ text_field.addEventListener("keypress", function(event) {
       block_to_insert.style.setProperty("text-decoration", "line-through");
     }
     block_to_insert.onmouseover = function(){
-      console.log("mouseover");
+      let delete_div = document.createElement('div');
+      let text=delete_div.innerHTML = '<i class="fa-solid fa-trash" style="color:white"></i>';
+      delete_div.classList.add('delete-todo');
+      delete_div.id = "delete-div";
+
+      let place_to_insert = document.getElementById('div-created');
+      place_to_insert.appendChild( delete_div );
+
     }
   }
 });
