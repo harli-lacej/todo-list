c770d876bd8a449f0f733ccda332231d93ce03fd
harli-lacej <harlilacej16@gmail.com>
2022-07-13 14:16:24 +0200

    changes in delete div and onmouseover event

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let text_field = document.getElementById("fname");
+let count = 0;
 text_field.addEventListener("keypress", function(event) {
   if(event.key === "Enter") {
-    //event.preventDefault();
-    console.log("Enter pressed");
+    count++;
+    console.log(count);
+    event.preventDefault();
     let block_to_insert = document.createElement('div');
-    block_to_insert.id = "div-created";
+    block_to_insert.id = "div-created"+count+"";
     let text_to_insert = document.getElementById('fname').value;
     let text=block_to_insert.innerHTML = ''+text_field.value;
     block_to_insert.classList.add('todo-div-todo');
@@ -15,15 +17,21 @@ text_field.addEventListener("keypress", function(event) {
     block_to_insert.onclick = function(){
       block_to_insert.style.setProperty("text-decoration", "line-through");
     }
-    block_to_insert.onmouseover = function(){
+      block_to_insert.onmouseover = function(){
       let delete_div = document.createElement('div');
       let text=delete_div.innerHTML = '<i class="fa-solid fa-trash" style="color:white"></i>';
       delete_div.classList.add('delete-todo');
-      delete_div.id = "delete-div";
+      delete_div.id = "delete-div"+count+"";
 
-      let place_to_insert = document.getElementById('div-created');
+      let place_to_insert = document.getElementById(block_to_insert.id);
       place_to_insert.appendChild( delete_div );
+    }
+    delete_div.onclick=function(){
+      const element = document.getElementById("delete-div"+count+"");
+      element.remove();
 
+      const element2 = document.getElementById("div-created"+count+"");
+      element2.remove();
     }
   }
 });
